6f125d183092f8fdc32937d47dafea5c0f99f747
LMENDOZA\LMendoza <lennin.mendoza@requordit.com>
2018-03-21 17:05:42 -0600

    Logs Issue fixed

--- CfdiService/CfdiService/Model/Logs.cs
+++ CfdiService/CfdiService/Model/Logs.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace CfdiService.Model
 {
     public enum UserTypes { User = 1, Employee = 2, Client = 3, Uploader = 4 }
-    public enum OperationTypes { UserCreated = 1, EmployeeCreated = 2, ClientUserCreated = 3, DocumentStored = 4, SignedBy = 5 }
+    public enum OperationTypes { UserCreated = 1, EmployeeCreated = 2, ClientUserCreated = 3, DocumentStored = 4, SignedBy = 5,
+            UserUpdated = 6, EmployeeUpdated = 7, ClientUserUpdated = 8, DocumentUpdated = 9 }
     public enum ObjectTypes { None = 0, Batch = 1, Client = 2, ClientUser = 3, Company = 4, Document = 5, Employee = 6, EmployeeCode = 7,
         EmployeeSecurityQuestion = 8, SignaturePurchase = 9, User = 10 }
 
     public class Logs
     {
+        [Key]
         public int LogId { get; set; }
         public OperationTypes Type { get; set; }
         public string Comments { get; set; }
