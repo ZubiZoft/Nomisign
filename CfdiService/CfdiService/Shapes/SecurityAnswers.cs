411b9c699103df58c026e71286e772ac10c4a74f
ABALDERAS <arturo.balderas@requordit.com>
2018-03-24 14:50:07 -0600

    Security questions, login/reset method changes.

---
+++ CfdiService/CfdiService/Shapes/SecurityAnswers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CfdiService.Shapes
+{
+    public class SecurityAnswers
+    {
+        public string Email { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+    }
+}
